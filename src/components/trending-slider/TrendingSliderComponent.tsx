b1eb27202a0ac72ed7a1617099c62580fa16040f
VladiSH31 <vladislav.sheremet@ukr.net>
2025-08-11 12:50:06 +0300

    feat: Implement and style TrendingSlider component
    - Add Swiper.js library to the project.
    - Create a reusable TrendingSliderComponent for the home page.
    - Implement slider with navigation, pagination, and loop functionality using Swiper React components and modules.
    - Move all component-specific styles to a dedicated CSS module (`.module.css`) for clean code and style encapsulation.
    - Customize Swiper's theme colors using CSS variables to match the application's accent color, ensuring a consistent design.

---
+++ src/components/trending-slider/TrendingSliderComponent.tsx
@@ -0,0 +1,40 @@
+import {type FC} from 'react';
+import type {IMovie} from "../../models/IMovie.ts";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination } from 'swiper/modules';
+import styles from './TrendingSliderComponent.module.css';
+
+
+type TrendingMoviesProps = {
+    trendingMovies: IMovie[];
+}
+
+
+const TrendingSliderComponent:FC<TrendingMoviesProps> = ({trendingMovies}) => {
+    return (
+        <Swiper
+            modules={[Navigation, Pagination]}
+            navigation
+            pagination={{ clickable: true }}
+            loop={true}>
+            {
+                trendingMovies.map(movie =>
+                    <SwiperSlide key={movie.id} className={styles.slide}>
+
+                    <img
+                        src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+                        alt={`${movie.title}`}
+                        className={styles.slideImage}
+                    />
+
+                    <div className={styles.slideContent}>
+                        <h2 className={styles.slideTitle}>{movie.title}</h2>
+                    </div>
+
+                </SwiperSlide>)
+            }
+        </Swiper>
+    );
+};
+
+export default TrendingSliderComponent;
--- src/pages/home-page/HomePage.tsx
+++ src/pages/home-page/HomePage.tsx
@@ -1,9 +1,28 @@
 import './HomePage.css'
+import {useEffect} from "react";
+import {homeSLiceAction} from "../../redux/store/slices/homeSlice.ts";
+import {useAppDispatch} from "../../redux/hooks/useAppDispatch.tsx";
+import {useAppSelector} from "../../redux/hooks/useAppSelector.tsx";
+import TrendingSliderComponent from "../../components/trending-slider/TrendingSliderComponent.tsx";
 
 const HomePage = () => {
+    const dispatch = useAppDispatch();
+    const {trendingMovie,status, error} = useAppSelector(state => state.homeSlice)
+
+    useEffect(() => {
+        dispatch(homeSLiceAction.loadHomePageContent())
+    }, [dispatch]);
+
     return (
         <div>
-            Home Page
+            <h1>Home Page</h1>
+            <h2>Status: {status}</h2>
+            {error && <h3>Error: {error}</h3>}
+
+
+            {status === 'succeeded' && (
+                <TrendingSliderComponent trendingMovies={trendingMovie} />
+            )}
         </div>
     );
 };
