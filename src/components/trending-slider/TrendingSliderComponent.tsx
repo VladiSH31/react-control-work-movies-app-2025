ec06cf6515a1605c3174c195a7537ea579e3ec28
VladiSH31 <vladislav.sheremet@ukr.net>
2025-08-12 12:52:31 +0300

    feat: add navigation link to movie details from trending slider

--- src/components/trending-slider/TrendingSliderComponent.tsx
+++ src/components/trending-slider/TrendingSliderComponent.tsx
@@ -1,8 +1,9 @@
 import {type FC} from 'react';
 import type {IMovie} from "../../models/IMovie.ts";
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination } from 'swiper/modules';
+import {Swiper, SwiperSlide} from 'swiper/react';
+import {Navigation, Pagination} from 'swiper/modules';
 import styles from './TrendingSliderComponent.module.css';
+import {Link} from "react-router-dom";
 
 
 type TrendingMoviesProps = {
@@ -10,28 +11,28 @@ type TrendingMoviesProps = {
 }
 
 
-const TrendingSliderComponent:FC<TrendingMoviesProps> = ({trendingMovies}) => {
+const TrendingSliderComponent: FC<TrendingMoviesProps> = ({trendingMovies}) => {
     return (
         <Swiper
             modules={[Navigation, Pagination]}
             navigation
-            pagination={{ clickable: true }}
+            pagination={{clickable: true}}
             loop={true}>
             {
                 trendingMovies.map(movie =>
                     <SwiperSlide key={movie.id} className={styles.slide}>
+                        <Link to={`/movie/${movie.id}`} className={styles.slideLink}>
+                            <img
+                                src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+                                alt={`${movie.title}`}
+                                className={styles.slideImage}
+                            />
 
-                    <img
-                        src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
-                        alt={`${movie.title}`}
-                        className={styles.slideImage}
-                    />
-
-                    <div className={styles.slideContent}>
-                        <h2 className={styles.slideTitle}>{movie.title}</h2>
-                    </div>
-
-                </SwiperSlide>)
+                            <div className={styles.slideContent}>
+                                <h2 className={styles.slideTitle}>{movie.title}</h2>
+                            </div>
+                        </Link>
+                    </SwiperSlide>)
             }
         </Swiper>
     );
